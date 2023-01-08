7e40b7c2001a942e6fa6e6798bb200d2f502b37a
Samuel Caetité <sscaetite@gmail.com>
2023-01-07 22:45:46 -0300

    reafactor: change code of home tiles

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,13 +1,17 @@
 (() => {
+    const convertToRGB = (hexColor) => {
+        var aRgbHex = hexColor.match(/.{1,2}/g);
+        var aRgb = `rgba(${parseInt(aRgbHex[0], 16)}, ${parseInt(aRgbHex[1], 16)}, ${parseInt(aRgbHex[2], 16)}, 0.2)`;
+        return aRgb;
+    }
     const elements = document.querySelectorAll(".tiles article")
+
     elements.forEach(el => {
-        const childSpan = el.querySelector('span')
-        const childImg = el.querySelector('img')
-        const imgUrl = childImg.getAttribute('src')
+        console.log(convertToRGB(el.getAttribute('data-overlay-color')))
         el.style.height = el.offsetWidth + 'px'
-        el.style.backgroundImage = 'url(' + imgUrl + ')' 
+        // el.style.backgroundImage = 'linear-gradient(' + convertToRGB(el.getAttribute('data-overlay-color')) + '), url(' + el.getAttribute('data-image-bg') + ')'
+        el.style.backgroundImage = 'url(' + el.getAttribute('data-image-bg')
         el.style.backgroundSize = 'cover'
         el.style.backgroundPosition = 'center'
-        childSpan.remove()
     })
 })()
