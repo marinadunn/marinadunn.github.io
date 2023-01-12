f42c92d418d03e526493caa6e0c1e54e2d138888
Samuel Caetité <sscaetite@gmail.com>
2023-01-11 21:03:40 -0300

    refactor: modify code of tiles at home

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,17 +1,23 @@
 (() => {
     const convertToRGB = (hexColor) => {
-        var aRgbHex = hexColor.match(/.{1,2}/g);
-        var aRgb = `rgba(${parseInt(aRgbHex[0], 16)}, ${parseInt(aRgbHex[1], 16)}, ${parseInt(aRgbHex[2], 16)}, 0.2)`;
-        return aRgb;
+        var aRgbHex = hexColor.replace('#', '').match(/.{1,2}/g)
+        var aRgb = `rgba(${parseInt(aRgbHex[0], 16)}, ${parseInt(aRgbHex[1], 16)}, ${parseInt(aRgbHex[2], 16)}, 0.6)`
+        return aRgb
     }
     const elements = document.querySelectorAll(".tiles article")
 
     elements.forEach(el => {
-        console.log(convertToRGB(el.getAttribute('data-overlay-color')))
+        const hexColor = convertToRGB(el.getAttribute('data-overlay-color'))
         el.style.height = el.offsetWidth + 'px'
-        // el.style.backgroundImage = 'linear-gradient(' + convertToRGB(el.getAttribute('data-overlay-color')) + '), url(' + el.getAttribute('data-image-bg') + ')'
-        el.style.backgroundImage = 'url(' + el.getAttribute('data-image-bg')
+        el.style.backgroundImage = `linear-gradient(to bottom, ${hexColor}, ${hexColor}),` + 'url(' + el.getAttribute('data-image-bg')
         el.style.backgroundSize = 'cover'
         el.style.backgroundPosition = 'center'
+        el.style.transition = 'all ease 0.9s !important'
+        el.addEventListener('mouseover', () => {
+            el.style.backgroundImage = 'url(' + el.getAttribute('data-image-bg')
+        })
+        el.addEventListener('mouseout', () => {
+            el.style.backgroundImage = `linear-gradient(to bottom, ${hexColor}, ${hexColor}),` + 'url(' + el.getAttribute('data-image-bg')
+        })
     })
 })()
